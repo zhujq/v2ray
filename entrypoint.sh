aa6b98e559f13ccd7276a23df09d8d52dff59b38
zhujq <38308874+zhujq@users.noreply.github.com>
2022-01-08 00:16:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ nohup /wechat-db &
 nohup /wechat-index &
 
 cd /v2ray
-wget -O v2ray.zip http://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+wget -O v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.23.2/v2ray-linux-64.zip
 unzip v2ray.zip 
 if [ ! -f "v2ray" ]; then
   mv /v2ray/v2ray-v$VER-linux-64/v2ray .
