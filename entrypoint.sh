ce1608a9446b9d80505cd7a5a4aae438fa246ba8
zhujq <14065567@qq.com>
2026-07-31 01:01:31 +0000

    h

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,13 @@
 
 cd /v2ray
 wget -O v2ray.zip http://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip v2ray.zip 
+unzip v2ray.zip
 if [ ! -f "v2ray" ]; then
   mv /v2ray/v2ray-v$VER-linux-64/v2ray .
   mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
   mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
   mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
 fi
-
 cp -f /config.json .
 chmod +x v2ray
 V2RAY_VMESS_AEAD_FORCED=false ./v2ray run
